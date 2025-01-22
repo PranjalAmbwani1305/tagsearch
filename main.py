8f1a854ddd200e7e620dca8920d1b6b75d0862f3
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2025-01-22 18:28:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
+st.title('Gujarati Newspaper Web Scraping')
 
-st.title('Web Scraping using BeautifulSoup')
+url = 'https://www.gujaratsamachar.com/'  # Change this to the actual URL
+st.write(f"Scraping content from: {url}")
 
-url = "https://epaper.gujaratsamachar.com/"
+tag = st.text_input('Enter the tag to search for (e.g., h1, p):', 'h1')
+
+  
