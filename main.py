47846646b225ea33be64cf6dddf6c01a39e48d5e
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2025-01-23 11:33:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def extract_article(link):
 
 def main():
     st.set_page_config(page_title="Gujarati News Article Scraper", page_icon="📰")
-    st.title("Gujarati News Article Scraper")
+    st.title("Gujarati News Article Finder")
     st.write("Search for articles on **Gujarat Samachar** using a keyword and extract their content dynamically.")
 
     base_url = "https://www.gujarat-samachar.com/"
